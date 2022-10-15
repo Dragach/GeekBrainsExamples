50d60aa9764652bdaf85723f1b6c5029a7185def
Dragach <dragach@MacBook-Pro-user1.local>
2022-10-15 15:05:36 +0300

    if else конструкции

---
+++ example5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username == "Женя")
+{
+    Console.WriteLine("Привет, капитан!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+  
